62dd1d53ad1938705f2ca7f4e7da79229e9cf699
malinac02 <malinacalarco@gmail.com>
2023-01-08 23:45:59 -0800

    completed project

---
+++ build/precache-manifest.2a625e2791727fcd43b4c6132c6463fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8586417e1b20f5f105f3bd24b11281f0",
+    "url": "/~malinac/index.html"
+  },
+  {
+    "revision": "bf9a0226c5e466106324",
+    "url": "/~malinac/static/css/main.7f65c8d4.chunk.css"
+  },
+  {
+    "revision": "a6f63fa8e0abd4a042a4",
+    "url": "/~malinac/static/js/2.bb7dfd9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/~malinac/static/js/2.bb7dfd9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9a0226c5e466106324",
+    "url": "/~malinac/static/js/main.fea95cf3.chunk.js"
+  },
+  {
+    "revision": "bb479cbe29fedff5308f",
+    "url": "/~malinac/static/js/runtime-main.e011af26.js"
+  }
+]);
